0145d60ad3044e07d60ee4223fde173f0b628e8f
KKFL tba <kkfltba2@gmail.com>
2023-03-27 10:57:37 +0300

    0.0.0.1

--- HelpDeskAi/Form1.cs
+++ HelpDeskAi/Form1.cs
@@ -16,5 +16,17 @@ namespace HelpDeskAi
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Credits credits = new Credits();
+            credits.Show();
+            this.Hide();
+        }
     }
 }
